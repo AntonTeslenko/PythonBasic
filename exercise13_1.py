f77ec186a1bc818ea1c1dcf92473819d78578a58
Anton Teslenko <antonteslenko89@gmail.com>
2024-11-22 15:55:24 +0100

    add exercise13_1.py

---
+++ exercise13_1.py
@@ -0,0 +1,114 @@
+class Human:
+    """
+    Клас, що описує людину.
+    """
+
+    def __init__(self, gender, age, first_name, last_name):
+        """
+        Ініціалізація об'єкта людини.
+        :param gender: Стать людини.
+        :param age: Вік людини.
+        :param first_name: Ім'я.
+        :param last_name: Прізвище.
+        """
+        self.gender = gender
+        self.age = age
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __str__(self):
+        """
+        Повертає інформацію про людину.
+        """
+        return f"{self.first_name} {self.last_name}, {self.gender}, {self.age} років"
+
+
+class Student(Human):
+    """
+    Клас, що описує студента, на основі класу Human.
+    """
+
+    def __init__(self, gender, age, first_name, last_name, record_book):
+        """
+        Ініціалізація об'єкта студента.
+        :param record_book: Номер залікової книжки.
+        """
+        super().__init__(gender, age, first_name, last_name)
+        self.record_book = record_book
+
+    def __str__(self):
+        """
+        Повертає інформацію про студента.
+        """
+        return f"{super().__str__()}, Залікова книжка: {self.record_book}"
+
+
+class Group:
+    """
+    Клас, що описує групу студентів.
+    """
+
+    def __init__(self, number):
+        """
+        Ініціалізація об'єкта групи.
+        :param number: Назва або номер групи.
+        """
+        self.number = number
+        self.group = set()  # Використовуємо множину для унікальності студентів.
+
+    def add_student(self, student):
+        """
+        Додає студента до групи.
+        :param student: Екземпляр класу Student.
+        """
+        self.group.add(student)
+
+    def find_student(self, last_name):
+        """
+        Шукає студента за прізвищем.
+        :param last_name: Прізвище студента.
+        :return: Екземпляр Student або None, якщо студент не знайдений.
+        """
+        for student in self.group:
+            if student.last_name == last_name:
+                return student
+        return None
+
+    def delete_student(self, last_name):
+        """
+        Видаляє студента за прізвищем.
+        :param last_name: Прізвище студента.
+        """
+        student = self.find_student(last_name)
+        if student:
+            self.group.remove(student)
+
+    def __str__(self):
+        """
+        Повертає список студентів у вигляді рядка.
+        """
+        all_students = "\n".join(str(student) for student in self.group)
+        return f"Група: {self.number}\n{all_students}"
+
+
+# Тестування
+st1 = Student('Male', 30, 'Steve', 'Jobs', 'AN142')
+st2 = Student('Female', 25, 'Liza', 'Taylor', 'AN145')
+gr = Group('PD1')
+gr.add_student(st1)
+gr.add_student(st2)
+
+# Виведення інформації про групу
+print(gr)
+
+# Тести на пошук студента
+assert str(gr.find_student('Jobs')) == str(st1), 'Test1'
+assert gr.find_student('Jobs2') is None, 'Test2'
+assert isinstance(gr.find_student('Jobs'), Student) is True, 'Метод пошуку має повертати екземпляр'
+
+# Видалення студентів
+gr.delete_student('Taylor')
+print(gr)  # Лише один студент залишився
+
+# Спроба видалення неіснуючого студента
+gr.delete_student('Taylor')  # Не повинно бути помилок
